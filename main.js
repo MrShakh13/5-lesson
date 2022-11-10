cb225960c3baff3b15f519e6e94a33681c863f4e
MrShakh13 <mrshahobiddin13@gmail.com>
2022-11-10 20:27:15 +0500

    changed mian.js!

---
+++ main.js
@@ -0,0 +1,30 @@
+// let list = [10, 20, "hello", true, NaN];
+// list.unshift(100);
+// list.pop();
+// list.shift();
+// list[2] = "Salom";
+// let obj = {
+// name: "Kuchukcha Oybek",
+// age: 21,
+// gender: "male",
+// };
+// obj.name = "shuhrat";
+// obj.age = 22;
+// obj.nationality = "uzbek";
+// delete obj.name;
+
+
+// console.log(obj);
+
+// let a = prompt("biror bir son kiriting:");
+
+// if (a > 0 && a < 10) {
+//     console.log(a, " son[0:10]");
+// } else if (a >= 10 && a <= 100) {
+//     console.log(a, " son[10:100]");
+// } else {
+//     console.log(a, "- bu son 1000 dan katta");
+// }
+for (let i = 10; i < 10; i++) {
+    console.log("hello world!");
+}
